cb98924232dacd54adc1eb384cd2de78ed99add6
Antonio Martinez <antoniomartinez@Mac.attlocal.net>
2025-06-04 01:15:58 -0500

    Remove build artifacts and update gitignore

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.DEE_3rGr.js","_app/immutable/chunks/CHeXQ_f2.js","_app/immutable/chunks/lywDo29W.js","_app/immutable/chunks/uwxEmWdT.js","_app/immutable/chunks/I6EhAnsj.js","_app/immutable/chunks/CGmsecmz.js"];
-export const stylesheets = ["_app/immutable/assets/0.ColubbVL.css"];
-export const fonts = [];
--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.CVLCHjsh.js","_app/immutable/chunks/CHeXQ_f2.js","_app/immutable/chunks/lywDo29W.js","_app/immutable/chunks/uwxEmWdT.js","_app/immutable/chunks/B4KrYGE1.js","_app/immutable/chunks/I6EhAnsj.js","_app/immutable/chunks/CGmsecmz.js","_app/immutable/chunks/Bh9cTnYZ.js","_app/immutable/chunks/BpMsfc0y.js"];
-export const stylesheets = [];
-export const fonts = [];
